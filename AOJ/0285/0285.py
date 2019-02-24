97be03a312acff0ad6013f8519824534fa61e216
Yutaka Watanobe <y.watanobe@gmail.com>
2019-02-24 16:47:18 +0900

    0285

--- AOJ/0285/0285.py
+++ AOJ/0285/0285.py
@@ -1,15 +1,15 @@
-#def parse(j, y, p, J, Y):
-#    if j == J and y == Y:
-#        print(p)
-#        return
-#    elif j == 5 and y <= 3 or y == 5 and j <= 3:
-#        return
-#
-#    if  j > J or y > Y:
-#        return
-#    
-#    parse(j+1, y, p + "A", J)
-#    parse(j, y+1, p + "B", Y) 
+def parse(j, y, p, J, Y):
+    if j == J and y == Y:
+        print(p)
+        return
+    elif j == 5 and y <= 3 or y == 5 and j <= 3:
+        return
+
+    if  j > J or y > Y:
+        return
+    
+    parse(j+1, y, p + "A", J, Y)
+    parse(j, y+1, p + "B", J, Y) 
 
 J,Y = map(int, input().split())
-#parse(0, 0, "", J, Y)
+parse(0, 0, "", J, Y)
