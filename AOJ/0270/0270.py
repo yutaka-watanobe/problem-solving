7018500822b154256b12d819599bfd6e7131e86d
Yutaka Watanobe <y.watanobe@gmail.com>
2019-03-17 19:52:11 +0900

    0270.py

---
+++ AOJ/0270/0270.py
@@ -0,0 +1,24 @@
+N, Q = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+M = max(C) + 1
+T = [0]*(M)
+for v in C: T[v] = 1
+
+L = [0]*(M)
+m = 0
+
+for i in range(M):
+    L[i] = m
+    if T[i] : m = i
+
+for i in range(Q):
+    q = int(input())
+    maxv = 0
+    cur = m
+    while 0 < cur:
+      p = cur % q
+      maxv = max(maxv, p)
+      if cur - p < 0:  break
+      cur = L[cur - p]
+    print(maxv)
