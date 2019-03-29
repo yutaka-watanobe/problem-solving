3465594676a6ded4af8554ebb713b6299f00b88a
Yutaka Watanobe <y.watanobe@gmail.com>
2019-03-29 11:31:20 +0900

    up

--- AOJ/0328/0328.py
+++ AOJ/0328/0328.py
@@ -1,22 +1,11 @@
-#include<iostream>
-using namespace std;
-static const int NMAX = 100000;
-static const int PMAX = 100000;
+PMAX = 100000
+N = int(input())
+T = [0 for i in range(2*PMAX+20)]
+for i in range(N):
+    ab = sum(map(int, input().split()))
+    T[ab] += 1
 
-int N, T[2*PMAX+20+1];
-
-int main(){
-  int a, b;
-  cin >> N;
-  for ( int i = 0; i < N; i++ ){
-    cin >> a >> b;
-    T[a+b]++;
-  }
-  for ( int i = 0; i < 2*PMAX+20; i++ ){
-    T[i+1] += T[i]/2;
-    T[i] = T[i]%2;
-    if ( T[i] ) cout << i << " " << 0 << endl;
-  }
-
-  return 0;
-}
+for i in range(2*PMAX+20-1):
+    T[i+1] += T[i]//2
+    T[i] = T[i]%2
+    if T[i] > 0 : print(i, 0)
