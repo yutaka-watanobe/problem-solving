a68ead20141985b4653f422dccd3e7a5e4dde1d4
Yutaka Watanobe <y.watanobe@gmail.com>
2019-03-29 11:54:40 +0900

    up

--- AOJ/0259/0259.cpp
+++ AOJ/0259/0259.cpp
@@ -4,17 +4,13 @@
 using namespace std;
 
 int compute(string num){
-
-  if ( num[0] == num[1] &&
-       num[1] == num[2] &&
-       num[2] == num[3] ) return -1;
+  if ( num[0] == num[1] && num[1] == num[2] && num[2] == num[3] ) return -1;
 
   int cnt = 0;
   string L, S;
   while(1){
     if ( num == "6174" ) break;
-    L = num;
-    S = num;
+    L = S = num;
     sort(L.begin(), L.end());
     reverse(L.begin(), L.end());
     sort(S.begin(), S.end());
@@ -30,13 +26,11 @@ int compute(string num){
 
 main(){
   string num;
-  int tcase = 0;
   while(1){
     cin >> num;
     if ( num == "0000" ) break;
-    tcase++;
     int a = compute(num);
-    if ( a< 0 ) cout << "NA" << endl;
+    if ( a < 0 ) cout << "NA" << endl;
     else cout << a << endl;
   }
 }
