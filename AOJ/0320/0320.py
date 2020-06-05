a2c7518376d726d7300fdb23da6f47d52ab9ddee
Yutaka Watanobe <y.watanobe@gmail.com>
2020-06-05 16:19:18 +0900

    up

--- AOJ/0320/0320.py
+++ AOJ/0320/0320.py
@@ -1,25 +1,23 @@
-def next(i):
+def next(N, i):
     return ((N-i-1)+N)%N
 
-def getState(G, i, j):
-    return  G[i][j] == G[i][next(j)] and  G[i][j] == G[next(i)][j] and G[i][j] == G[next(i)][next(j)]
+def getState(N, G, i, j):
+    return  G[i][j] == G[i][next(N, j)] and  G[i][j] == G[next(N, i)][j] and G[i][j] == G[next(N, i)][next(N, j)]
 
 def getInit(N, G):
     dcnt = 0
     for i in range(N//2):
         for j in range(N//2):
-            if not getState(G, i, j): dcnt += 1
+            if not getState(N, G, i, j): dcnt += 1
     return dcnt
 
 C, N = map(int, input().split())
-G = [[0]*N]*N
-
+#G = [[0]*N]*N
+G = [['N' for _ in range(N)] for _ in range(N)]
 for i in range(N):
     str = input()
-    j = 0
-    for ch in str:
-        G[i][j] = int(ch)
-        j += 1
+    for j in range(N):
+        G[i][j] = int(str[j])
 
 ans = 0
 dcnt = getInit(N, G)
@@ -31,11 +29,11 @@ for i in range(C-1):
         r, c = map(int, input().split())
         r -= 1
         c -= 1
-        pre = getState(G, r, c)
-        G[r][c] = 0 if G[r][c]=='1' else 1
-        post = getState(G, r, c)
+        pre = getState(N, G, r, c)
+        G[r][c] = 0 if G[r][c]== 1 else 1
+        post = getState(N, G, r, c)
         if not pre and post: dcnt -= 1
         elif  pre and not post: dcnt += 1
-        if dcnt == 0: ans += 1
+    if dcnt == 0: ans += 1
 
 print(ans)
