542b44c5fbb21433e90468f1e48155801788c55f
Yutaka Watanobe <y.watanobe@gmail.com>
2020-07-12 22:20:30 +0900

    add python

---
+++ AOJ/0325/0325.py
@@ -0,0 +1,13 @@
+def solve():
+    D = [[0,0]]*6
+    for i in range(6):
+        h, w = sorted(map(int,input().split()))
+        D[i] = [h, w]
+    D.sort()
+    for i in range(0,6,2):
+        if D[i] != D[i+1]: return Fals
+
+    return D[0][0] == D[2][0] and D[0][1] == D[4][0] and D[2][1] == D[4][1]
+
+if solve(): print('yes')
+else: print('no')
