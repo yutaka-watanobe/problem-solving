9c29b7e3e3d928ff141e915946d4a2c31e9d604a
Yutaka Watanobe <y.watanobe@gmail.com>
2020-07-12 22:22:19 +0900

    debug

--- AOJ/0325/0325.py
+++ AOJ/0325/0325.py
@@ -5,7 +5,7 @@ def solve():
         D[i] = [h, w]
     D.sort()
     for i in range(0,6,2):
-        if D[i] != D[i+1]: return Fals
+        if D[i] != D[i+1]: return False
 
     return D[0][0] == D[2][0] and D[0][1] == D[4][0] and D[2][1] == D[4][1]
 
