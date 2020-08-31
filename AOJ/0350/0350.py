7d22af7c529663ae36429c7c888001ab51cc8725
Yutaka Watanobe <y.watanobe@gmail.com>
2020-08-31 21:49:02 +0900

    update 0350

--- AOJ/0350/0350.py
+++ AOJ/0350/0350.py
@@ -6,23 +6,16 @@ def printV(x, y):
     print(str(x//g) + "/" +  str(y//g))
 
 S = input()
-all = ""
-sub = ""
-p = -1
-
-for i in range(len(S)):
-    if S[i] == '.':
-        o = i
-    elif S[i] == '(':
-        p = i
-        sub = all
-    elif S[i] != ')':
-        all += S[i]
-  
+o = S.find('.')
+p = S.find('(')
 d = len(S) - o - 1
-l = p - o - 1
+
 if p == -1:
+    all = S[0:o] + S[o + 1:len(S)]
     printV(int(all), 10**d)
 else:
+    sub = S[0:o] + S[o + 1:p]
+    all = sub + S[p + 1:len(S) - 1]
+    l = p - o - 1
     d -= 2 # for ()
     printV(int(all) - int(sub), 10**d - 10**l)
