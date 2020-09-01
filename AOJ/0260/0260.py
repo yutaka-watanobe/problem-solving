8adaedacc89a6475a7033a34803ba334ec4f8cc2
Yutaka Watanobe <y.watanobe@gmail.com>
2020-09-01 22:25:00 +0900

    0260

--- AOJ/0260/0260.py
+++ AOJ/0260/0260.py
@@ -1,3 +1,21 @@
+MAX = 65000
+def merge(J, l, m, r):
+    T = []*MAX
+    for i in range(l, m): T[i] = J[i]
+    for i in range(m, r): T[(r-1)-(i-m)] = J[i]
+    i = l
+    j = r - 1
+    for k in range(l, r):
+        if  T[i] >= T[j]: J[k] = T[i]; i += 1
+        else: J[k] = T[j]; j -= 1
+
+def mergeSort(J, l, r):
+    if l+1 >= r: return
+    m = (l + r) / 2
+    mergeSort(J, l, m)
+    mergeSort(J, m, r)
+    merge(J, l, m, r)
+
 while True:
     n = int(input())
     if n == 0: break
@@ -10,15 +28,16 @@ while True:
 
     maxv = 0
     J.sort(reverse=True)
-    J.append(0); # nazo
+    J.append(0); 
     totalj = 0
-    t = n
-    j = 0
+    t = n; j = 0
     while t >= 1:
         v = t*(totalj + totalp)
         totalj += J[j]
         maxv = max(v, maxv)
-        t = t - 1
-        j = j + 1
+        t -= 1; j += 1
 
     print(maxv)
+
+
+
