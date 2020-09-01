ac6baa766524bad594504ef03a5d53773c07b5d4
Yutaka Watanobe <y.watanobe@gmail.com>
2020-09-01 20:48:20 +0900

    update 0325

--- AOJ/0325/0325.py
+++ AOJ/0325/0325.py
@@ -1,13 +1,20 @@
+N = 6
 def solve():
-    D = [[0,0]]*6
-    for i in range(6):
+    D = [[0,0]]*N
+    for i in range(N):
         h, w = sorted(map(int,input().split()))
         D[i] = [h, w]
-    D.sort()
-    for i in range(0,6,2):
+    # bubble sort 
+    for i in range(N-2):
+        j = N - 2
+        while j >= i:
+            if D[j] > D[j+1]: D[j], D[j+1] = D[j+1], D[j]
+            j -= 1
+    for i in range(0, N, 2):
         if D[i] != D[i+1]: return False
 
     return D[0][0] == D[2][0] and D[0][1] == D[4][0] and D[2][1] == D[4][1]
 
-if solve(): print('yes')
-else: print('no')
+print("yes" if solve() else "no")
+
+
