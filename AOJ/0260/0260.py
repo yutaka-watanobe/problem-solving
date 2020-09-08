6244303c14512871fec657832dc7e3754af98fcf
Yutaka Watanobe <y.watanobe@gmail.com>
2020-09-08 14:48:17 +0900

    update 0260.py

--- AOJ/0260/0260.py
+++ AOJ/0260/0260.py
@@ -1,6 +1,6 @@
 MAX = 65000
+T = [0]*MAX
 def merge(J, l, m, r):
-    T = []*MAX
     for i in range(l, m): T[i] = J[i]
     for i in range(m, r): T[(r-1)-(i-m)] = J[i]
     i = l
@@ -11,7 +11,7 @@ def merge(J, l, m, r):
 
 def mergeSort(J, l, r):
     if l+1 >= r: return
-    m = (l + r) / 2
+    m = (l + r) // 2
     mergeSort(J, l, m)
     mergeSort(J, m, r)
     merge(J, l, m, r)
@@ -27,8 +27,8 @@ while True:
     J = list(map(int, input().split()))
 
     maxv = 0
-    J.sort()
-    J.append(0); 
+    mergeSort(J, 0, n - 1)
+    J.append(0) 
     totalj = 0
     for k in range(0, n):
         totalj += J[n - 1 - k]
