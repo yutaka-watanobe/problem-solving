f40e3cde28194e6c03427400039302d8f4e21c19
Yutaka Watanobe <y.watanobe@gmail.com>
2020-09-14 14:09:20 +0900

    update 0259

--- AOJ/0259/0259.py
+++ AOJ/0259/0259.py
@@ -1,14 +1,14 @@
 while True:
-    num = input()
-    if int(num) == 0: break
-    if len(set(num)) == 1:
+    N = input()
+    if int(N) == 0: break
+    if len(set(N)) == 1:
         print("NA")
         continue
     cnt = 0
-    while num != "6174":
+    while N != "6174":
+        L = ''.join(sorted(N))
+        S = ''.join(reversed(sorted(N)))
+        N = str(int(S) - int(L)).zfill(4)
         cnt += 1
-        L = ''.join(sorted(num))
-        S = ''.join(reversed(sorted(num)))
-        num = str(int(S) - int(L)).zfill(4)
     print(cnt)
     
