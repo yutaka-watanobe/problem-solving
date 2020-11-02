f9bcff0381c9e26b6f7b3218fbe32109f67455f2
Yutaka Watanobe <y.watanobe@gmail.com>
2020-11-03 00:27:41 +0900

    up

--- AOJ/0325/0325.c
+++ AOJ/0325/0325.c
@@ -4,25 +4,26 @@
 struct Rect{ int h, w; } ;
 
 int solve(){
-  struct Rect D[N], tmp;
-  for ( int i = 0; i < N; i++ ) {
+  int i, j, tmp;
+  struct Rect D[N], rtmp;
+  for ( i = 0; i < N; i++ ) {
     scanf("%d %d", &D[i].h, &D[i].w);
     if ( D[i].h > D[i].w) {
-      int tmp = D[i].h; D[i].h = D[i].w; D[i].w = tmp;
+      tmp = D[i].h; D[i].h = D[i].w; D[i].w = tmp;
     }
   }
 
   /* バブルソート */
-  for ( int i = 0; i < N - 2; i++ ){
-    for ( int j = N - 2; j >= i; j-- ){
+  for ( i = 0; i < N - 2; i++ ){
+    for ( j = N - 2; j >= i; j-- ){
       if ( D[j].h > D[j + 1].h ||
 	   D[j].h == D[j + 1].h && D[j].w > D[j + 1].w ){
-	   tmp = D[j]; D[j] = D[j + 1]; D[j + 1] = tmp;
+	rtmp = D[j]; D[j] = D[j + 1]; D[j + 1] = rtmp;
       }
     }
   }
 
-  for ( int i = 0; i < N; i += 2 )
+  for ( i = 0; i < N; i += 2 )
     if ( D[i].h != D[i + 1].h || D[i].w != D[i + 1].w ) return 0;
 
   return D[0].h == D[2].h && D[0].w == D[4].h && D[2].w == D[4].w;
--- AOJ/0346/0346.c
+++ AOJ/0346/0346.c
@@ -3,23 +3,25 @@
 int e[N];
 
 int check(){
+  int i, j, minj, t;
   /* 選択ソート */
-  for ( int i = 0; i < N - 1; i++ ){
-    int minj = i;
-    for ( int j = i + 1; j < N; j++ )
+  for ( i = 0; i < N - 1; i++ ){
+    minj = i;
+    for ( j = i + 1; j < N; j++ )
       if ( e[j] < e[minj] ) minj = j;
-    int t = e[minj];
+    t = e[minj];
     e[minj] = e[i];
     e[i] = t;
   }
   
-  for ( int i = 0; i < N; i += 4)
+  for ( i = 0; i < N; i += 4)
     if ( e[i] != e[i + 3] ) return 0;
   return 1;
 }
 
 int main(){
-  for ( int i = 0; i < N; i++ ) scanf("%d", &e[i]);
+  int i;
+  for ( i = 0; i < N; i++ ) scanf("%d", &e[i]);
   printf(check() ? "yes\n": "no\n");
   return 0;
 }
