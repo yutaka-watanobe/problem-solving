2bd3b51d4438acd914e3aaaa6435f90d5469f2cc
Yutaka Watanobe <y.watanobe@gmail.com>
2020-11-03 08:53:09 +0900

    up

--- AOJ/0347/0347.py
+++ AOJ/0347/0347.py
@@ -1,18 +1,18 @@
-n = int(input())
-S = list(map(int, input().split()))
+N = int(input())
+A = list(map(int, input().split()))
 res = 0.0;
-S.sort()
+A.sort()
 
-for d in range(n-1):
-    c = d+1
-    a = n-1
-    b = n-2
-    if d == n-3:
-        b = d-1
-    elif d == n-2:
-        a = d-2
-        b = d-1
-    res = max(res, 1.0*(S[a]+S[b])/(S[c]-S[d]))
-
+for p4 in range(N - 1):
+    p3 = p4 + 1
+    p1 = N - 1
+    p2 = N - 2
+    if p4 == N - 3:
+        p2 = p4 - 1
+    elif p4 == N - 2:
+        p1 = p4 - 2
+        p2 = p4 - 1
+    res = max(res, 1.0 * \
+              (A[p1] + A[p2]) / (A[p3] - A[p4]))
 print(res)
 
